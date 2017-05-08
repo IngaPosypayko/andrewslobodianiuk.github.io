6db9c1221732d1eee9ac42f7abb8d03728e3db60
Andrew Slobodianiuk <speshmillo@gmail.com>
2017-05-08 20:02:27 +0300

    new

---
+++ Js_homework_1/js/script.js
@@ -0,0 +1,86 @@
+'use strict';
+
+
+var number = prompt('Enter the number');
+var degree = prompt('Set a degree');
+
+function MathPow(numberStatic, degreeStatic) {
+    var number = numberStatic;
+    var result;
+
+    if (degreeStatic == 0) {
+        return 1;
+    }
+    else {
+
+        if (degreeStatic > 0) {
+            for (var i = 1; i <= degreeStatic - 1; i++) {
+                result = number * numberStatic;
+                number = result;
+
+            }
+            return result;
+
+        } else {
+            degreeStatic = degreeStatic * (-1);
+                for (var i = 1; i <= degreeStatic - 1; i++) {
+                    result = number * numberStatic;
+                    number = result;
+
+                }
+                return 1/result;
+
+        }
+    }
+
+
+}
+
+
+console.log(MathPow(number, degree));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
