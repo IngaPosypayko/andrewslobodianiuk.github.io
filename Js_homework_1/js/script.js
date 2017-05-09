d00f06c00a1b8c53a57fdd907dedd4a26132116b
Andrew Slobodianiuk <speshmillo@gmail.com>
2017-05-09 18:37:01 +0300

    new

--- Js_homework_1/js/script.js
+++ Js_homework_1/js/script.js
@@ -5,12 +5,14 @@ var number = prompt('Enter the number');
 var degree = prompt('Set a degree');
 
 function mathPow(numberStatic, degreeStatic) {
+
     var number = numberStatic;
     var result;
 
     if (degreeStatic == 0) {
         return 1;
     }
+        
     else {
         if (degreeStatic > 0) {
             for (var i = 1; i <= degreeStatic - 1; i++) {
@@ -28,7 +30,6 @@ function mathPow(numberStatic, degreeStatic) {
         }
     }
 }
-
 
 console.log(mathPow(number, degree));
 
--- js_homework_2/js/scripts.js
+++ js_homework_2/js/scripts.js
@@ -1,32 +1,24 @@
-/**
- * Created by andreyslobodanyk on 09.05.17.
- */
+'use strict';
 
 var listOfNames = [];
-
 for (var i = 0; i < 5; i++) {
-
     listOfNames.push(prompt('Enter the name'));
-
 }
 
 var enteredName = prompt('Submit your name');
 
 for (var i = 0; i < listOfNames.length; i++) {
-
-    var nameFromList = listOfNames[i];
-
-        if (enteredName == nameFromList){
+    
+    var nameFromList = listOfNames[i].toLowerCase();
+        if (enteredName.toLowerCase() == nameFromList){
             break
         }
 }
 
-if (enteredName == nameFromList) {
-
+if (enteredName.toLowerCase() == nameFromList) {
     alert(enteredName + ', вы успешно вошли!');
 
 } else {
-
     alert('Ошибка');
 
 }
