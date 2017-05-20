a2158cc33e75a336ef4d2a28d67287a5fbdcf7b2
Andrew Slobodianiuk <speshmillo@gmail.com>
2017-05-20 12:56:59 +0300

    new

--- js_homework_2/js/scripts.js
+++ js_homework_2/js/scripts.js
@@ -10,18 +10,23 @@ var enteredName = prompt('Submit your name');
 
 for (var i = 0; i < listOfNames.length; i++) {
 
-        if (enteredName.toLowerCase() == listOfNames[i].toLowerCase()){
-            break;
-        }
+    if (enteredName.toLowerCase() === listOfNames[i].toLowerCase()){
+        break;
+    }
 }
 
-if (enteredName.toLowerCase() == listOfNames[i].toLowerCase()) {
+if (enteredName.toLowerCase() === listOfNames[i].toLowerCase()) {
     alert(enteredName + ', вы успешно вошли!');
 
 } else {
     alert('Ошибка');
 
 }
+
+
+
+
+
 
 
 
