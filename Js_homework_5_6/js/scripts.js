e595df979b7227488d6ccad431fbd16b791ae4d7
Andrew Slobodianiuk <speshmillo@gmail.com>
2017-05-29 20:51:51 +0300

    new

--- Js_homework_5_6/js/scripts.js
+++ Js_homework_5_6/js/scripts.js
@@ -1,15 +1,53 @@
 'use strict';
 
-var time = +new Date();
 
-var time2 = +new Date(2017,4,25,16,30);
+(function timer() {
 
-var test3 = time2 - time;
+    var div = document.createElement('div');
+    div.id = 'timer';
+    document.body.appendChild(div);
 
-var test4 = new Date(test3);
+    document.getElementById('timer').innerHTML = 'Hello';
+
+    var start = new Date();
+
+    setInterval(function () {
+        var deltaTime = (Date.now() - start);
+
+        var msecMinute = 1000 * 60;
+        var msecHour = msecMinute * 60;
+        var msecDay = msecHour * 24;
+
+        var day = Math.floor(deltaTime / msecDay);
+        deltaTime = deltaTime - ( day * msecDay);
+
+        var hour = Math.floor(deltaTime / msecHour);
+        deltaTime = deltaTime - (hour * msecHour);
+
+        var min = Math.floor(deltaTime / msecMinute);
+        deltaTime = deltaTime - ( min * msecMinute);
+
+        var sec = Math.floor(deltaTime / 1000);
+
+        deltaTime = deltaTime - sec * 1000;
+
+        var msec = Math.floor(deltaTime);
+
+        console.log('days ' + day, ':', 'hours ' + hour, ':', 'min ' + min, ':', 'sec ' + sec, ':', 'msec ' + msec);
+
+    }, 1);
 
 
-alert(test4);
+} )();
+
+
+
+
+
+
+
+
+
 
 
 
