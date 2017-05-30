35c98fc24a1c2295f2d0c9d08340cb7135d5efa3
Andrew Slobodianiuk <speshmillo@gmail.com>
2017-05-30 17:38:09 +0300

    new

--- Js_homework_5_6/js/scripts.js
+++ Js_homework_5_6/js/scripts.js
@@ -1,44 +1,134 @@
 'use strict';
 
 
-(function timer() {
+(function () {
+
+        var timer = document.createElement('div');
+    timer.setAttribute('class', 'timer');
+    document.body.appendChild(timer);
 
     var div = document.createElement('div');
-    div.id = 'timer';
-    document.body.appendChild(div);
+    div.setAttribute('id', 'timer');
+    timer.appendChild(div);
 
-    document.getElementById('timer').innerHTML = 'Hello';
+    var button = document.createElement('button');
+    timer.appendChild(button);
+    button.appendChild(document.createTextNode('Старт'));
+    button.className = 'btn btn-default';
 
-    var start = new Date();
+    var buttonClear = document.createElement('button');
+    timer.appendChild(buttonClear);
+    buttonClear.appendChild(document.createTextNode('Очистить'));
+    buttonClear.className = 'btn btn-default';
 
-    setInterval(function () {
-        var deltaTime = (Date.now() - start);
-
-        var msecMinute = 1000 * 60;
-        var msecHour = msecMinute * 60;
-        var msecDay = msecHour * 24;
-
-        var day = Math.floor(deltaTime / msecDay);
-        deltaTime = deltaTime - ( day * msecDay);
-
-        var hour = Math.floor(deltaTime / msecHour);
-        deltaTime = deltaTime - (hour * msecHour);
-
-        var min = Math.floor(deltaTime / msecMinute);
-        deltaTime = deltaTime - ( min * msecMinute);
-
-        var sec = Math.floor(deltaTime / 1000);
-
-        deltaTime = deltaTime - sec * 1000;
-
-        var msec = Math.floor(deltaTime);
-
-        console.log('days ' + day, ':', 'hours ' + hour, ':', 'min ' + min, ':', 'sec ' + sec, ':', 'msec ' + msec);
-
-    }, 1);
+    document.getElementById('timer').innerHTML = '00:00:00:00:000';
 
 
-} )();
+    button.addEventListener ('click', function () {
+        var start = new Date();
+
+        var interval = setInterval(function () {
+            var deltaTime = (Date.now() - start);
+
+            var msecMinute = 1000 * 60;
+            var msecHour = msecMinute * 60;
+            var msecDay = msecHour * 24;
+
+            var day = Math.floor(deltaTime / msecDay);
+            deltaTime = deltaTime - ( day * msecDay);
+
+            var hour = Math.floor(deltaTime / msecHour);
+            deltaTime = deltaTime - (hour * msecHour);
+
+            var min = Math.floor(deltaTime / msecMinute);
+            deltaTime = deltaTime - ( min * msecMinute);
+
+            var sec = Math.floor(deltaTime / 1000);
+
+            deltaTime = deltaTime - sec * 1000;
+
+            var msec = Math.floor(deltaTime);
+
+            if (min < 10) {
+                min = '0' + min;
+            }
+
+            if (hour < 10) {
+                hour = '0' + hour;
+            }
+
+            if (day < 10) {
+                day = '0' + day;
+            }
+
+            if (sec < 10) {
+                sec = '0' + sec;
+            }
+
+            document.getElementById('timer').innerHTML = day + ':' + hour + ':' + min + ':' + sec + ':' + msec;
+
+
+        }, 1);
+
+        buttonClear.addEventListener('click', function () {
+            clearInterval(interval);
+            document.getElementById('timer').innerHTML = '00:00:00:00:000';
+            button.innerHTML = 'Старт';
+
+        });
+
+        button.innerHTML = 'Пауза';
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
