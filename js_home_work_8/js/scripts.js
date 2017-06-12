40d32fd17fd13ac1e743e8f5d5c559546719e810
Andrew Slobodianiuk <speshmillo@gmail.com>
2017-06-12 18:55:16 +0300

    new

--- js_home_work_8/js/scripts.js
+++ js_home_work_8/js/scripts.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+var $inputs = $('.input'),
+    $p = $('p');
+
+
+$p.css('opacity', '0');
+
+
+
+$inputs.on('mouseover', function () {
+
+    $p.animate({
+        'opacity': '1'
+
+    });
+
+
+});
