eedb8b9dd03a3a7383506c56ecc2ce6589c75e47
AndrewSlobodianiuk <speshmillo@gmail.com>
2017-06-12 12:20:48 +0300

    new

--- js_home_work_8/js/scripts.js
+++ js_home_work_8/js/scripts.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+
+
+
+
+
+var $p = $('p');
+
+$p.css('opacity', '0');
+
+
+$('.login').on('mouseover', function () {
+
+    $('.login_hint').css('opacity', '1');
+
+});
+
+$('.login').on('mouseout', function () {
+
+    $('.login_hint').css('opacity', '0');
+
+});
+
+
+
+
+
