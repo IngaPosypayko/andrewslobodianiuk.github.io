bdd51334e49ee9956fb9fb629bd08e30a5cba766
AndrewSlobodianiuk <speshmillo@gmail.com>
2017-06-12 16:48:42 +0300

    new

--- js_home_work_8/js/scripts.js
+++ js_home_work_8/js/scripts.js
@@ -2,26 +2,89 @@
 
 
 
+$('tooltip').css('opacity', '0');
+
+
+
+$('.login').mousemove (function () {
+
+    var $dataTooltip = $(this).attr('data-tooltip');
+
+
+    $('.tooltip').text($dataTooltip).css({
+        'opacity': 1,
+        'left': 260,
+        'top': 0
+    });
+
+
+})
+    .mouseout (function () {
+        $('.tooltip').css({
+                'opacity': 0
+
+        }
+        );
+
+
+    });
+
+
+$('.pass').mousemove (function () {
+
+    var $dataTooltip = $(this).attr('data-tooltip');
+
+
+    $('.tooltip').text($dataTooltip).css({
+        'left': 460,
+        'top': 455
+    })
+
+        .show();
+})
+    .mouseout (function () {
+        $('.tooltip').hide();
+
+    });
+
+$('.pass_confirm').mousemove (function () {
+
+    var $dataTooltip = $(this).attr('data-tooltip');
+
+
+    $('.tooltip').text($dataTooltip).css({
+        'left': 460,
+        'top': 480
+
+    })
+
+        .show();
+})
+    .mouseout (function () {
+        $('.tooltip').hide();
+
+    });
 
 
 
 
-var $p = $('p');
-
-$p.css('opacity', '0');
 
 
-$('.login').on('mouseover', function () {
 
-    $('.login_hint').css('opacity', '1');
 
-});
 
-$('.login').on('mouseout', function () {
 
-    $('.login_hint').css('opacity', '0');
 
-});
+
+
+
+
+
+
+
+
+
+
 
 
 
