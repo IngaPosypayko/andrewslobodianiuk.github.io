891454d6083373f4e2784bbcf53d4357743422b0
AndrewSlobodianiuk <speshmillo@gmail.com>
2017-06-14 15:13:23 +0300

    new

--- js_home_work_8/js/scripts.js
+++ js_home_work_8/js/scripts.js
@@ -19,17 +19,22 @@ $('.input').mousemove (function () {
             {
                 'left': $inputPosition.left+220,
                 'top': $inputPosition.top+5
-            })
+            }).queue('fx', [])
             .fadeIn('slow');
 })
     .mouseout (function () {
-        $('.onAction').removeClass('onAction').prev().fadeOut().removeClass('onMove').addClass('tooltip');
+        $('.onAction').removeClass('onAction')
+            .prev()
+                .queue('fx', []).fadeOut()
+                    .removeClass('onMove')
+                        .addClass('tooltip');
+
 
     });
 
 
 $('.submit').on('click', function () {
-
+    $('.tooltip').stop();
 
 
     $('#tooltip_1').text($('#input_1').attr('data-tooltip'))
@@ -37,20 +42,23 @@ $('.submit').on('click', function () {
             {
                 'left': $('#input_1').position().left+220,
                 'top': $('#input_1').position().top+5
-            })  .fadeIn('slow');
+            })
+                .fadeIn('slow');
     $('#tooltip_2').text($('#input_2').attr('data-tooltip'))
         .css(
             {
                 'left': $('#input_2').position().left+220,
                 'top': $('#input_2').position().top+5
-            })  .fadeIn('slow');
+            })
+                .fadeIn('slow');
 
     $('#tooltip_3').text($('#input_3').attr('data-tooltip'))
         .css(
             {
                 'left': $('#input_3').position().left+220,
                 'top': $('#input_3').position().top+5
-            })  .fadeIn('slow');
+            })
+                .fadeIn('slow');
 
 });
 
