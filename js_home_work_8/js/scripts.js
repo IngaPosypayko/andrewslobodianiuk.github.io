152a5b9a504f49c679b9308761a41b4efd7a6408
AndrewSlobodianiuk <speshmillo@gmail.com>
2017-06-14 11:57:43 +0300

    new

--- js_home_work_8/js/scripts.js
+++ js_home_work_8/js/scripts.js
@@ -2,30 +2,60 @@
 
 $('.tooltip').hide();
 
-$('[data-tooltip]').mousemove (function () {
 
-    $('div.tooltip').toggleClass('tooltip onMove');
+
+
+$('.input').mousemove (function () {
+
+    $('.tooltip').hide(1000);
+
+    $(this).parent().addClass('test');
+
+    $('.test').prev().removeClass('tooltip').addClass('onMove');
 
      var $dataTooltip = $(this).attr('data-tooltip');
 
-     var $inputPosition = $(this).position();
+    var $inputPosition = $(this).position();
 
         $('.onMove').text($dataTooltip).css(
             {
                 'left': $inputPosition.left+220,
                 'top': $inputPosition.top+5
             })
-            .show();
+            .show(1000);
 })
     .mouseout (function () {
-        $('.onMove').hide();
+        $('.test').removeClass('test').prev().removeClass('onMove').addClass('tooltip');
+        $('.tooltip').hide(1000);
+
     });
 
 
-$('.submit').on('click', function () {
-    $('div.onMove').toggleClass('tooltip onMove');
 
-    $('.tooltip').show();
+
+
+$('.submit').on('click', function () {
+
+
+    $('#tooltip_1').text($('#input_1').attr('data-tooltip'))
+        .css(
+            {
+                'left': $('#input_1').position().left+220,
+                'top': $('#input_1').position().top+5
+            }) .show(1000);
+    $('#tooltip_2').text($('#input_2').attr('data-tooltip'))
+        .css(
+            {
+                'left': $('#input_2').position().left+220,
+                'top': $('#input_2').position().top+5
+            }) .show(1000);
+
+    $('#tooltip_3').text($('#input_3').attr('data-tooltip'))
+        .css(
+            {
+                'left': $('#input_3').position().left+220,
+                'top': $('#input_3').position().top+5
+            }) .show(1000);
 
 });
 
