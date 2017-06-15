372a4201e31f5a05c1bee339193538ff4d6d5842
AndrewSlobodianiuk <speshmillo@gmail.com>
2017-06-15 16:12:32 +0300

    new

--- js_home_work_8/js/scripts.js
+++ js_home_work_8/js/scripts.js
@@ -1,37 +1,26 @@
 'use strict';
 
-$('.tooltip').css({
+
+
+var $tooltips = $('.tooltip'),
+    $inputs = $('.input');
+
+$tooltips.css({
     'opacity': 0
 });
 
 
-$('.input').mousemove (function () {
+$inputs.mousemove (function () {
 
     $('.tooltip').css({
         'opacity': 0
     });
 
-    $(this).parent().addClass('onAction');
-
-    $('.onAction').prev().removeClass('tooltip').addClass('onMove');
-
-     var $dataTooltip = $(this).attr('data-tooltip');
-
-    var $inputPosition = $(this).position();
-
-        $('.onMove').text($dataTooltip).css(
-            {
-                'left': $inputPosition.left+220,
-                'top': $inputPosition.top+5
-            })
-            .stop().fadeTo(100, 1);
+        $(this).next().stop().fadeTo(100, 1);
 })
     .mouseout (function () {
-        $('.onAction').removeClass('onAction')
-            .prev()
-            .stop().fadeTo(100, 0)
-                    .removeClass('onMove')
-                        .addClass('tooltip');
+        $(this).next()
+                .stop().fadeTo(100, 0);
 
     });
 
@@ -39,20 +28,7 @@ $('.input').mousemove (function () {
 
 
 $('.submit').on('click', function () {
-
-    function tooltip(tooltipId, inputId) {
-        var tooltip = $(tooltipId);
-        var input = $(inputId);
-        tooltip.text(input.attr('data-tooltip'))
-            .css({
-                'left': input.position().left + 220,
-                'top': input.position().top + 5
-            })
-            .stop().fadeTo(100, 1);
-    }
-    tooltip('#tooltip_1', '#input_1');
-    tooltip('#tooltip_2', '#input_2');
-    tooltip('#tooltip_3', '#input_3');
+    $tooltips.stop().fadeTo(100, 1);
 
 
 });
